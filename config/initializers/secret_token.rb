f9f4c9ad7c6353194dc0be29e1288f3b94304de8
talhait07 <monsur.it07@gmail.com>
2014-11-25 01:10:12 +0600

    dsaf

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Travellers::Application.config.secret_key_base = '21ade6b2cfe609d394eb6dff1d2e7c642360c7866a6398a6251c6b6041cd182cd0b77eddd93ab337ff1e31812fa94c9b5b3ac7d13a5af08303414aa6a6f748ca'
